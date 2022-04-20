190ff24f072404eb20b2e77d7b1ec4c51c1168c4
eyagudin <eyagudin@phoenixit.ru>
2022-04-20 22:40:41 +0300

    added requests

--- src/test/scala/Rosim/Requests.scala
+++ src/test/scala/Rosim/Requests.scala
@@ -5,6 +5,8 @@ import io.gatling.http.Predef._
 
 
 object Requests {
+  //feeders
+  val users = csv("data/users.csv").circular
 
 
   //Rosim requests
@@ -58,86 +60,158 @@ object Requests {
     .get("/public/dashboard/privatizedObjects?year=2021")
     .check(status.is(200))
 
-  //  val addressSearchString = http("listCountries") // getting countries by string
-  //    .get(ports("ADDRESS") + s"/secured/address/country?characters=" + randomString(3))
-  //    .check(status.is(200))
-  //
-  //  val addressSearchCode = http("listCountries") // searching address by code
-  //    .get(ports("ADDRESS") + s"/secured/address/country/" + randomString(3))
-  //    .check(status.is(200))
-  //
-  //  val login = feed(users) // data from users.csv, login
-  //    .exec(http("Login")
-  //      .post(ports("AUTH") + "/public/auth/login").body(StringBody(
-  //      """{
-  //        |
-  //        |    "phoneNumber": "${log}",
-  //        |
-  //        |    "password": "${pass}",
-  //        |
-  //        |    "deviceId": "the_second_one"
-  //        |}""".stripMargin)).asJson
-  //      .check(status.is(200))
-  ////      .check(jmesPath("data.token").saveAs("token")) //extracting token from login response
-  //    )
-  //  val userInfo = http("userInfo") // getting user profile info
-  //
-  //    .get("/secured/user/")
-  //    .header("Authorization", "Bearer " + s"${token}")
-  //    .check(status.is(200))
-  //  val swapOutlet = feed(outlets) // data from outlets.csv, outlet
-  //    .exec(http("swapOutlet") // changing user's outlet
-  //    .put(ports("AUTH") + "/secured/user/change/outlet").body(StringBody(
-  //    """ ${outlet}
-  //      """.stripMargin)).asJson
-  //    .header("Authorization", "Bearer " + s"${token}")
-  //    .check(status.is(200))
-  //      .check(status.is(409))
-  //    )
-  //
-  //  val refreshToken = http("refreshToken") // getting user profile info
-  //    .post(ports("AUTH") + "/public/auth/token/refresh").body(StringBody(
-  //    """s{
-  //      "accessToken":${token}"
-  //      }""".stripMargin)).asJson
-  //    .check(status.is(200))
-  //    .check(status.is(401))
-  //
-  //  val logEvent = http("userInfo") // registration some events
-  //    .post(ports("REPORT") + "/secured/log/event").body(StringBody(
-  //    """{
-  //          "eventType": "SELL_ATTEMPT",
-  //    "eventDateTime": "2021-04-23T12:43:06.164Z",
-  //    "geolocation": "test_geolocation",
-  //    "addInformationField": "test_information"
-  //      }""".stripMargin)).asJson
-  //    .check(status.is(200))
-  //    .header("Authorization", "Bearer " + s"${token}")
-  //
-  //  val listDocuments = http("listDocuments") // get list mandatory docs
-  //    .get(ports("TELEDEALER") + "/public/fs/template_document/list")
-  //    .check(status.is(200))
-  //
-  //  val checkCode = feed(users) // data from users.csv
-  //    .exec(http("checkCode") // checking sms code
-  //    .get(ports("SMS") + "/public/sms/check/code?code=" + randomNumber.nextInt(10000) + "&msisdn=${log}")
-  //    .check(status.is(409))
-  //    )
-  //  val simAllowance = http("simAllowance") // checking a sim allowance for sale
-  //
-  //    .post(ports("SIM") + "/secured/simcard/saleAvailable/check").body(StringBody(
-  //    s"""{
-  //    "name":${randomString(6)},
-  //    "surname":${randomString(6)},
-  //    "documentNumber":${randomString(6)}
-  //}""".stripMargin)).asJson
-  //    .header("Authorization", "Bearer " + s"${token}")
-  //    .check(status.is(200))
-  //
-  //  val logout = http("logout") // execute logout
-  //    .get(ports("AUTH") + "/secured/auth/logout")
-  //    .header("Authorization", "Bearer " + s"${token}")
-  //    .check(status.is(200))
+  val regionMapAggregate = http("regionMapAggregate")
+    .get("/public/datamart/getRegionMapAggregate")
+    .check(status.is(200))
+
+  val regionMapAggregateParameterized = feed(Array(
+    Map("region" -> "RU-SA"),
+  ).circular)
+    .exec(http("dashboardObjectsBudget")
+      .get("/public/datamart/getRegionMapAggregate/${region}")
+      .check(status.is(200)))
+
+  val districtMapAggregate = http("districtMapAggregate")
+    .get("/public/filter/municipalDistricts")
+    .check(status.is(200))
+  val npaContent = http("npaContent")
+    .get("/public/content/laws")
+    .check(status.is(200))
+
+  val faqContent = http("faqContent")
+    .get("/public/content/faq")
+    .check(status.is(200))
+
+  val sectionInfo = http("sectionInfo")
+    .get("/public/content/sectionInfo")
+    .check(status.is(200))
+
+  val sendMessageItsm = http("sendMessageItsm")
+    .post("/public/message/itsm/send").formParam("name","subject")
+    .check(status.is(200))
+
+  val sendMessageItsmFilesIncluded = http("sendMessageItsmFilesIncluded")
+    .post("/public/message/itsm/send").formParam("name","subject")
+    .formUpload("filename","file.pdf") // Здесь в filePath указать путь (относительный) до файла, в папке data
+    .check(status.is(200))
+
+  val facilityRentalList = http("facilityRentalList")
+    .post("/public/datamart/facilityRental/list?sort=&page=0&size=10")
+    .body(StringBody("""{
+  "subjectCodes": [
+    "RU-CHE"
+  ],
+  "types": [
+    "LK_OBJECT_TYPE_LAND_PLOT"
+  ],
+  "categoryCode": [
+    "RI_LAND_AGRICULTURAL",
+    "RI_LAND_PROTECTED_TERRITORIES",
+    "RI_LAND_SPECIAL_PURPOSE",
+    "secretData",
+    "RI_LAND_WITHOUT_CATEGORY"
+  ],
+  "permittedUsageFormCode": [
+    "RI_LAND_TYPE_1015",
+    "RI_LAND_TYPE_1011",
+    "RI_LAND_TYPE_1013",
+    "RI_LAND_TYPE_1003",
+    "RI_LAND_TYPE_1006",
+    "RI_LAND_TYPE_1002",
+    "RI_LAND_TYPE_1005",
+    "RI_LAND_TYPE_1001",
+    "RI_LAND_TYPE_1000",
+    "RI_LAND_TYPE_1007",
+    "RI_LAND_TYPE_1004",
+    "RI_LAND_TYPE_1010",
+    "RI_LAND_TYPE_1016",
+    "RI_LAND_TYPE_1008",
+    "RI_LAND_TYPE_1009",
+    "RI_LAND_TYPE_1012",
+    "RI_LAND_TYPE_1014",
+    "RI_LAND_TYPE_-1000"
+  ]
+}"""))
+    .check(status.is(200))
+
+  val facilityRental = http("facilityRental")
+    .get("/public/datamart/facilityRental?id=3246344a-d5be-473f-8021-60ef5b67c7f6")
+    .check(status.is(200))
+
+  // url другой + нужен токен наверное для этой группы
+  val techNotifications = http("techNotifications")
+    .get("/content/secured/techNotifications/actual")
+    .check(status.is(200))
+
+  val confirmNotificationsDocuments = http("confirmNotifications")
+    .get("/secured/user/messages/confirmation/documents?esiaUserId=1000466580")
+    .check(status.is(200))
+
+  val countUnreadMessages = http("countUnreadMessages")
+    .get("/secured/user/messages/unread/count?esiaUserId=1000466580")
+    .check(status.is(200))
+
+  val userSubscriptions = feed(users)
+  .exec(http("userSubscriptions")
+    .get("/secured/subscriptions/user/${userId}")
+    .check(status.is(200)))
+
+  val userDrafts = feed(users)
+    .exec(http("userDrafts")
+      .get("/secured/lifeSituations/drafts/${userId}")
+      .check(status.is(200)))
+
+
+  val insertDraft = http("insertDraft")
+    .post("/secured/lifeSituations/saveDraft?userId=1000466580")
+    .body(StringBody("""{
+  "appealParams": {
+    "appealType": "BUY_WITHOUT_BIDDING"
+  },
+  "data": {
+    "juridicalName": "ОРГАНИЗАЦИЯ 2107516123",
+    "ogrn": "1196658017500",
+    "inn": "0000000000",
+    "fullName": "Иванов Иван Иванович",
+    "email": "test@test.test",
+    "cadastralNumber": "74:37:0209001:0410",
+    "registrationContractCode": "REGISTRATION_CONTRACT_ROSIM",
+    "realEstateNumberList": [
+      null
+    ],
+    "decisionList": [],
+    "decisionData": {},
+    "article": [
+      {
+        "value": "ARTICLE_4",
+        "label": "пп. 10 п. 2 статьи 39.3 ЗК РФ Без проведения торгов осуществляется продажа земельных участков гражданам для индивидуального жилищного строительства, ведения личного подсобного хозяйства в границах населенного пункта, садоводства, гражданам или крестьянским (фермерским) хозяйствам для осуществления крестьянским (фермерским) хозяйством его деятельности в соответствии со статьей 39.18 настоящего Кодекса"
+      }
+    ],
+    "deliveryMethod": {
+      "value": "DELIVERY_2",
+      "label": "Почтой"
+    },
+    "position": {
+      "value": "POSITION_1",
+      "label": "Директор"
+    },
+    "senderPhone": "88005553535",
+    "senderAddress": "127434, Город Москва, Улица Дубки ",
+    "senderEmail": "test@test.test",
+    "agency": {
+      "value": "RI_TU_106",
+      "label": "МТУ в Республике Мордовия‚ Республике Марий Эл‚ Чувашской Республике и Пензенской области"
+    },
+    "totalSquare": 8,
+    "address": "положениеместо",
+    "permittedUsageForm": {
+      "value": "RI_LAND_TYPE_1013",
+      "label": "Земельные участки, занятые особо охраняемыми территориями и объектами, городскими лесами, скверами, парками, городскими садами"
+    },
+    "objectsMissing": true
+  }
+}"""))
+    .check(status.is(200))
 }
 
 
--- src/test/scala/Rosim/rosim/BasicSimulation.scala
+++ src/test/scala/Rosim/rosim/BasicSimulation.scala
@@ -27,20 +27,8 @@ class BasicSimulation extends Simulation {
 //  val records: Seq[Map[String, Any]] = csv("./src/test/resources/data/users.csv").readRecords
   val scn =
     scenario(f"load test") //
-      .exec(dashboardObjectsBudget)
-//      .pause(2) // записал паузы в реальном времени
-//      .exec(login)
-//      .exec(userInfo)
-//      .exec(swapOutlet)
-//      .exec(addressSearchString)
-//      .exec(addressSearchCode)
-//      .exec(logEvent)
-//      .exec(checkCode)
-//      .exec(refreshToken)
-//      .exec(listDocuments)
-//      .exec(simAllowance)
-//      .exec(logout)
-//
-  setUp(scn.inject(constantUsersPerSec(1) during(5 seconds)).protocols(httpProtocol))
+      .exec(facilityRentalList)
+
+  setUp(scn.inject(constantUsersPerSec(1) during(1 seconds)).protocols(httpProtocol))
 
 }
